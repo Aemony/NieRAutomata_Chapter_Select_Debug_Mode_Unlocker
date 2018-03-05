e8f168d6712cb705a6dc94ed7125aae8330b27d3
Idearum <idearum@outlook.com>
2018-03-05 11:34:51 +0100

    Add project files.

---
+++ NieRAutomata_ChapterSelect-DebugMode_Unlocker/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace NieRAutomata_ChapterSelect_DebugMode_Unlocker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
+            this.buttonOpenSlot = new System.Windows.Forms.Button();
+            this.cbDebugMode = new System.Windows.Forms.CheckBox();
+            this.cbChapterSelectDebugRoom = new System.Windows.Forms.CheckBox();
+            this.cbUnknown1 = new System.Windows.Forms.CheckBox();
+            this.cbUnknown2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Enabled = false;
+            this.buttonSave.Location = new System.Drawing.Point(269, 153);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 8;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(8, 48);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(336, 32);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "4 bits stores the flags to enable Chapter Select/Debug Room and the Debug Mode, a" +
+    "nd two other unknown states.";
+            // 
+            // textBoxFilePath
+            // 
+            this.textBoxFilePath.Location = new System.Drawing.Point(89, 12);
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.ReadOnly = true;
+            this.textBoxFilePath.Size = new System.Drawing.Size(255, 20);
+            this.textBoxFilePath.TabIndex = 2;
+            this.textBoxFilePath.Text = "No slot file loaded...";
+            // 
+            // buttonOpenSlot
+            // 
+            this.buttonOpenSlot.Location = new System.Drawing.Point(8, 10);
+            this.buttonOpenSlot.Name = "buttonOpenSlot";
+            this.buttonOpenSlot.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenSlot.TabIndex = 1;
+            this.buttonOpenSlot.Text = "Open slot...";
+            this.buttonOpenSlot.UseVisualStyleBackColor = true;
+            this.buttonOpenSlot.Click += new System.EventHandler(this.buttonOpenSlot_Click);
+            // 
+            // cbDebugMode
+            // 
+            this.cbDebugMode.AutoSize = true;
+            this.cbDebugMode.Enabled = false;
+            this.cbDebugMode.Location = new System.Drawing.Point(8, 86);
+            this.cbDebugMode.Name = "cbDebugMode";
+            this.cbDebugMode.Size = new System.Drawing.Size(143, 17);
+            this.cbDebugMode.TabIndex = 4;
+            this.cbDebugMode.Text = "Debug Menu (RT+Back)";
+            this.cbDebugMode.UseVisualStyleBackColor = true;
+            // 
+            // cbChapterSelectDebugRoom
+            // 
+            this.cbChapterSelectDebugRoom.AutoSize = true;
+            this.cbChapterSelectDebugRoom.Enabled = false;
+            this.cbChapterSelectDebugRoom.Location = new System.Drawing.Point(157, 86);
+            this.cbChapterSelectDebugRoom.Name = "cbChapterSelectDebugRoom";
+            this.cbChapterSelectDebugRoom.Size = new System.Drawing.Size(170, 17);
+            this.cbChapterSelectDebugRoom.TabIndex = 5;
+            this.cbChapterSelectDebugRoom.Text = "Chapter Select / Debug Room";
+            this.cbChapterSelectDebugRoom.UseVisualStyleBackColor = true;
+            // 
+            // cbUnknown1
+            // 
+            this.cbUnknown1.AutoSize = true;
+            this.cbUnknown1.Enabled = false;
+            this.cbUnknown1.Location = new System.Drawing.Point(157, 109);
+            this.cbUnknown1.Name = "cbUnknown1";
+            this.cbUnknown1.Size = new System.Drawing.Size(88, 17);
+            this.cbUnknown1.TabIndex = 7;
+            this.cbUnknown1.Text = "Unknown #1";
+            this.cbUnknown1.UseVisualStyleBackColor = true;
+            // 
+            // cbUnknown2
+            // 
+            this.cbUnknown2.AutoSize = true;
+            this.cbUnknown2.Enabled = false;
+            this.cbUnknown2.Location = new System.Drawing.Point(8, 109);
+            this.cbUnknown2.Name = "cbUnknown2";
+            this.cbUnknown2.Size = new System.Drawing.Size(88, 17);
+            this.cbUnknown2.TabIndex = 6;
+            this.cbUnknown2.Text = "Unknown #2";
+            this.cbUnknown2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 188);
+            this.Controls.Add(this.cbUnknown2);
+            this.Controls.Add(this.cbUnknown1);
+            this.Controls.Add(this.cbChapterSelectDebugRoom);
+            this.Controls.Add(this.cbDebugMode);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBoxFilePath);
+            this.Controls.Add(this.buttonOpenSlot);
+            this.Name = "Form1";
+            this.Text = "NieR: Automata - Chapter Select + Debug Room/Menu Unlocker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxFilePath;
+        private System.Windows.Forms.Button buttonOpenSlot;
+        private System.Windows.Forms.CheckBox cbDebugMode;
+        private System.Windows.Forms.CheckBox cbChapterSelectDebugRoom;
+        private System.Windows.Forms.CheckBox cbUnknown1;
+        private System.Windows.Forms.CheckBox cbUnknown2;
+    }
+}
+
---
+++ NieRAutomata_ChapterSelect-DebugMode_Unlocker/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NieRAutomata_ChapterSelect_DebugMode_Unlocker
+{
+    public partial class Form1 : Form
+    {
+        /* REMEMBER LITTLE-ENDIAN! */
+        private enum Flag
+        {
+            None = 0x00, // 0000 0000
+            Unknown1 = 0x01, // 0000 0001
+            Unknown2 = 0x02, // 0000 0010
+            ChapterSelectAndDebugRoom = 0x04, // 0000 0100
+            DebugMenu = 0x08, // 0000 1000
+            Unused1 = 0x16, // 0001 0000
+            Unused2 = 0x32, // 0010 0000
+            Unused3 = 0x64, // 0100 0000
+            Unused4 = 0x128, // 1000 0000
+        }
+
+        private string _filePath = null;
+        private static int _intBlockOffset = 234775;
+        private Flag _enumFlags = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpenSlot_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = DialogResult.Yes;
+
+            if (_filePath != null)
+            {
+                dialogResult = MessageBox.Show("You already have a file opened. Opening another will discard all unsaved changes.\n\nAre you sure you want to continue?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+            }
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog
+                {
+                    InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\My Games\\NieR_Automata",
+                    Filter = "Save File (SlotData_#.dat)|SlotData_*.dat",
+                    Title = "Open slot file"
+                };
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    _filePath = openFileDialog.FileName;
+                    textBoxFilePath.Text = _filePath;
+                    textBoxFilePath.SelectionStart = textBoxFilePath.Text.Length;
+
+                    // Read the file
+                    using (Stream stream = openFileDialog.OpenFile())
+                    {
+                        stream.Position = _intBlockOffset;
+                        _enumFlags = (Flag)stream.ReadByte();
+                    }
+
+#if DEBUG
+                    Console.WriteLine("Read: " + _enumFlags);
+#endif
+
+                    // Populate the checkboxes based on the flags
+                    cbUnknown1.Checked = _enumFlags.HasFlag(Flag.Unknown1);
+                    cbUnknown2.Checked = _enumFlags.HasFlag(Flag.Unknown2);
+                    cbDebugMode.Checked = _enumFlags.HasFlag(Flag.DebugMenu);
+                    cbChapterSelectDebugRoom.Checked = _enumFlags.HasFlag(Flag.ChapterSelectAndDebugRoom);
+
+                    // Enable the GUI
+                    buttonSave.Enabled = true;
+                    cbUnknown1.Enabled = true;
+                    cbUnknown2.Enabled = true;
+                    cbChapterSelectDebugRoom.Enabled = true;
+                    cbDebugMode.Enabled = true;
+                }
+            }
+        }
+        
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to save your changes to the slot file?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+
+                _enumFlags = cbDebugMode.Checked ? _enumFlags | Flag.DebugMenu : _enumFlags & ~Flag.DebugMenu;
+                _enumFlags = cbChapterSelectDebugRoom.Checked ? _enumFlags | Flag.ChapterSelectAndDebugRoom : _enumFlags & ~Flag.ChapterSelectAndDebugRoom;
+                _enumFlags = cbUnknown1.Checked ? _enumFlags | Flag.Unknown1 : _enumFlags & ~Flag.Unknown1;
+                _enumFlags = cbUnknown2.Checked ? _enumFlags | Flag.Unknown2 : _enumFlags & ~Flag.Unknown2;
+
+#if DEBUG
+                Console.WriteLine("Wrote: " + Enum.Parse(typeof(Flag), _enumFlags.ToString()));
+#endif
+
+                // Create a backup first
+                File.Copy(_filePath, _filePath + DateTime.Now.ToString("_yyyy-MM-dd_HH-mm-ss.bak"));
+
+                // Now overwrite the data
+                using (Stream stream = File.Open(_filePath, FileMode.Open, FileAccess.Write))
+                {
+                    stream.Position = _intBlockOffset;
+                    stream.WriteByte((byte)_enumFlags);
+                }
+
+                MessageBox.Show("Changes were saved!", "Save complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
